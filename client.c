4c100795fc8bcb01139b7de08f051d27387507c4
Hamlet zheng <1025157436@qq.com>
2018-01-08 16:47:44 +0800

    poll : add files

---
+++ client.c
@@ -0,0 +1,90 @@
+/* client.c --- 
+ * Filename: client.c
+ * Created: Mon Jan  8 16:45:33 2018 (+0800)
+ */
+
+/* Copyright Hamlet_Jiaxiaoyu.
+ * 
+ * 允许免费使用，拷贝，修改，发布，但在所有的拷贝上必须保留上述
+ * copyright部分和本使用声明部分，除非显示声明，copyright的持有者
+ * 不得作为再发布软件的广告。copyright的持有者对使用本软件的适用范围不做任何声明，
+ * 
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <poll.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define MAXLINE     1024
+#define IPADDRESS   "127.0.0.1"
+#define SERV_PORT   8787
+
+#define max(a,b) (a > b) ? a : b
+
+static void handle_connection(int sockfd);
+
+int main(int argc,char *argv[])
+{
+    int                 sockfd;
+    struct sockaddr_in  servaddr;
+    sockfd = socket(AF_INET,SOCK_STREAM,0);
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET,IPADDRESS,&servaddr.sin_addr);
+    connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+    //处理连接描述符
+    handle_connection(sockfd);
+    return 0;
+}
+
+static void handle_connection(int sockfd)
+{
+    char    sendline[MAXLINE],recvline[MAXLINE];
+    int     maxfdp,stdineof;
+    struct pollfd pfds[2];
+    int n;
+    //添加连接描述符
+    pfds[0].fd = sockfd;
+    pfds[0].events = POLLIN;
+    //添加标准输入描述符
+    pfds[1].fd = STDIN_FILENO;
+    pfds[1].events = POLLIN;
+    for (; ;)
+    {
+        poll(pfds,2,-1);
+        if (pfds[0].revents & POLLIN)
+        {
+            n = read(sockfd,recvline,MAXLINE);
+            if (n == 0)
+            {
+                    fprintf(stderr,"client: server is closed.\n");
+                    close(sockfd);
+            }
+            write(STDOUT_FILENO,recvline,n);
+        }
+        //测试标准输入是否准备好
+        if (pfds[1].revents & POLLIN)
+        {
+            n = read(STDIN_FILENO,sendline,MAXLINE);
+            if (n  == 0)
+            {
+                shutdown(sockfd,SHUT_WR);
+        continue;
+            }
+            write(sockfd,sendline,n);
+        }
+    }
+}
